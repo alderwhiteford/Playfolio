599d91522ef568bf04fb5eb79cb5ae2ae5998e8a
Alder Whiteford <alderjwhiteford@gmail.com>
2024-06-29 14:08:32 -0400

    Add mobile menu (#4)
    * Menu on scroll
    
    * Add Menu and remove circle cursor on mobile

--- src/components/Dropdown/NavDropdown.tsx
+++ src/components/Dropdown/NavDropdown.tsx
@@ -14,14 +14,14 @@ export default function NavDropdown({ isVisible, cursorEnter, cursorLeave }: Nav
     return (
         <div>
             <div 
-                className={`right-16 top-16 fixed z-40 ${isVisible ? 'opacity-100 visible' : 'opacity-0 invisible'} ${isOpen ? 'rotate-90' : 'rotate-0'} transition-all ease-in-out`}
+                className={`right-8 top-8 md:right-16 md:top-16 fixed z-40 ${isVisible ? 'opacity-100 visible' : 'opacity-0 invisible'} ${isOpen ? 'rotate-90' : 'rotate-0'} transition-all ease-in-out`}
                 onMouseOver={cursorEnter}
                 onMouseLeave={cursorLeave}
                 onClick={() => setIsOpen(!isOpen)}
             >
                 <MenuIcon sx={{ color: 'white' }} fontSize={'large'}/>
             </div>
-            <div className={`${isOpen && isVisible ? 'opacity-100 visible' : 'opacity-0 invisible'} p-6 fixed z-40 right-16 top-28 bg-black bg-opacity-60 text-white border-white border-[1px] rounded-2xl flex flex-col items-end justify-center text-2xl font-thin`}>
+            <div className={`${isOpen && isVisible ? 'opacity-100 visible' : 'opacity-0 invisible'} p-6 fixed z-40 right-8 top-20 md:right-16 md:top-28 bg-black bg-opacity-60 text-white border-white border-[1px] rounded-2xl flex flex-col items-end justify-center text-2xl font-thin`}>
                 {LINKS.map((link) => {
                     return (
                         <Link 
