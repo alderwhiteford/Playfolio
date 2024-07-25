84e0849c59fe4247499fa1f4f131c9ea6aafc589
Alder Whiteford <alderjwhiteford@gmail.com>
2024-07-25 15:51:41 -0400

    Minor Project Clean-up

--- src/components/playfolio/Project/Project.tsx
+++ src/components/playfolio/Project/Project.tsx
@@ -1,12 +1,15 @@
-import Image, { StaticImageData } from "next/image";
+/* eslint-disable @next/next/no-img-element */
+import Image from "next/image";
 
 import GitHub from "@/assets/skillIcons/github.png";
+import InfoIcon from "@/assets/miscellaneous/infoIcon.png";
 import EyeIcon from "@/assets/miscellaneous/eyeIcon.png"
 import { SkillMappings } from "@/utilties/skillMappings";
 import Flip from "@/components/playfolio/Reveal/Flip";
 import { SectionProps } from "@/pages";
 import SkillIcon from "@/components/playfolio/Skill/SkillIcon";
 import Reveal from "../Reveal/Reveal";
+import PlayfolioTooltip from "../Tooltip/Tooltip";
 
 export type Skill = 'AWS' | 'Azure' | 'C++' | 'CSS' | 'Docker' | 'Firebase' | 'GCP' | 'Golang' 
             | 'HTML' | 'Java' | 'JavaScript' | 'MongoDB' | 'NextJS' | 'Postgres' | 'Python'
@@ -16,12 +19,13 @@ interface ProjectProps extends SectionProps {
     title: string,
     description: string,
     github?: string,
+    additionalInfo?: string,
     skills: Skill[],
-    video: StaticImageData,
+    video: string,
     liveLink?: string,
 }
 
-export default function Project({ title, description, github, skills, video, liveLink, cursorEnter, cursorLeave}: ProjectProps) {
+export default function Project({ title, description, github, additionalInfo, skills, video, liveLink, cursorEnter, cursorLeave}: ProjectProps) {
     return (
         <div className='grid grid-cols-1 lg:grid-cols-5 w-full gap-10 items-center lg:mt-10 lg:mb-10'>
             <div className='col-span-3 md:col-span-2'>
@@ -33,22 +37,44 @@ export default function Project({ title, description, github, skills, video, liv
                         {description}
                         <div className='flex flex-row gap-3 items-center'>
                             {github &&
-                            <>
-                                <a 
-                                    href={github}
-                                    target="_blank"
-                                    className='hidden lg:flex'
-                                    onMouseEnter={() => cursorEnter()}
-                                    onMouseLeave={() => cursorLeave()}
-                                >
-                                    <Image 
-                                        src={GitHub}
-                                        alt=''
-                                        className='w-6 lg:w-8'
-                                    />
-                                </a>
-                                <div className='h-[30px] w-[1.5px] bg-white mr-2 ml-2 hidden lg:flex'/>
-                            </>
+                                <>
+                                    <PlayfolioTooltip title='View Code'>
+                                        <a 
+                                            href={github}
+                                            target="_blank"
+                                            className='hidden lg:flex'
+                                            onMouseEnter={() => cursorEnter()}
+                                            onMouseLeave={() => cursorLeave()}
+                                        >
+                                            <Image 
+                                                src={GitHub}
+                                                alt=''
+                                                className='w-6 lg:w-8'
+                                            />
+                                        </a>
+                                    </PlayfolioTooltip>
+                                    <div className='h-[30px] w-[1.5px] bg-white mr-2 ml-2 hidden lg:flex'/>
+                                </>
+                            }
+                            {additionalInfo &&
+                                <>
+                                    <a 
+                                        href={additionalInfo}
+                                        target="_blank"
+                                        className='hidden lg:flex'
+                                        onMouseEnter={() => cursorEnter()}
+                                        onMouseLeave={() => cursorLeave()}
+                                    >
+                                        <PlayfolioTooltip title='More Information'>
+                                            <Image 
+                                                src={InfoIcon}
+                                                alt=''
+                                                className='w-6 lg:w-8'
+                                            />
+                                        </PlayfolioTooltip>
+                                    </a>
+                                    <div className='h-[30px] w-[1.5px] bg-white mr-2 ml-2 hidden lg:flex'/>
+                                </>
                             }
                             <div className='flex flex-row flex-wrap gap-3'>
                                 {skills.map((skill) =>
@@ -69,11 +95,13 @@ export default function Project({ title, description, github, skills, video, liv
                                         onMouseEnter={() => cursorEnter()}
                                         onMouseLeave={() => cursorLeave()}
                                     >
-                                        <Image 
-                                            src={EyeIcon}
-                                            alt=''
-                                            className='w-6 lg:w-8'
-                                        />
+                                        <PlayfolioTooltip title='View Live Site'>
+                                            <Image 
+                                                src={EyeIcon}
+                                                alt=''
+                                                className='w-6 lg:w-8'
+                                            />
+                                        </PlayfolioTooltip>
                                     </a>
                                 </>
                             }
@@ -83,10 +111,10 @@ export default function Project({ title, description, github, skills, video, liv
             </div>
             <div className='col-span-3 lg:pl-10'>
                 <Flip>
-                    <Image 
+                    <img 
                         src={video}
                         alt=''
-                        className='rounded-2xl w-full h-full'
+                        className='rounded-2xl w-full h-full max-h-[450px] object-cover object-top'
                     />
                 </Flip>
             </div>
